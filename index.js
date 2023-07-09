75524807d439af32c0ba3fe63b12454036ae8e9c
h-zahar <64719808+h-zahar@users.noreply.github.com>
2023-07-09 09:46:58 +0000

    Finalized Prototype

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  // res.json(Object.keys(req));
+  // const ipAddress = req.ip;
+  const ipAddress = req.headers['x-forwarded-for'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  
+  const myIdentity = {
+    ipaddress: ipAddress,
+    language,
+    software
+  };
+  
+  res.json(myIdentity);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
